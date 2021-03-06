4605ab499ae6c1c161d0ad8b6d321dbf6176f838
tamim11 <mmmtamim@gmail.com>
2021-03-06 22:00:05 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Review from './components/Review/Review';
 
 function App() {
     return (
         <div>
             <Header></Header>
-            <Shop></Shop>
+            <Router>
+                <Switch>
+                    <Route path="/shop">
+                        <Shop></Shop>
+                    </Route>
+                    <Route path="/review">
+                        <Review></Review>
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
